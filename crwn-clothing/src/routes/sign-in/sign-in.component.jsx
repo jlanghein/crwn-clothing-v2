6350e0cb471f2c769b13a0ff853df8c4c43be7d7
Johannes Langhein <johannes_langhein@gmx.de>
2022-10-06 21:21:12 -0400

    integrated firebase

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,18 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(user);
+  };
   return (
     <div>
-      <he>Sign In Page</he>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
 };
