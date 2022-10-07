2b7e0aade92c2b77c49ae63c08fe85a76876e5af
Johannes Langhein <johannes_langhein@gmx.de>
2022-10-07 15:09:01 -0400

    finished authentication

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -16,7 +16,7 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="signIn" to="/sign-in">
+          <Link className="signIn" to="/auth">
             SIGN IN
           </Link>
         </div>
